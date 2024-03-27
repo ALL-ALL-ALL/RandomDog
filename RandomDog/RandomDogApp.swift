66cbc7d1bed18d0903174f4d0e5ba012bf9fe91e
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-03-27 21:37:29 +0100

    Initial Commit

---
+++ RandomDog/RandomDogApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomDogApp.swift
+//  RandomDog
+//
+//  Created by  Ixart on 27/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct RandomDogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
