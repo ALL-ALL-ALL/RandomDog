ca7e91de7ca1b3e739da98287cda3d97867dbfbb
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-03-28 00:51:34 +0100

    en cours .....

--- RandomDog/ContentView.swift
+++ RandomDog/ContentView.swift
@@ -10,14 +10,48 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+            
+            ZStack {
+                
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    
+                    
+                    ZStack {
+                        Rectangle()
+                            .frame(width: 150, height: 80)
+                            .foregroundColor(.black)
+                            .cornerRadius(40)
+                        
+                        Image(systemName: "dog")
+                            .font(.system(size: 40))
+                            .foregroundColor(.white)
+
+                    } // fin zstack
+                    
+                }) // FIN LABEL
+                
+               
+
+                
+               
+
+                
+            }
+        } // fin  vstack
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    } // fin body
+} // fin strcut
 
 #Preview {
     ContentView()
